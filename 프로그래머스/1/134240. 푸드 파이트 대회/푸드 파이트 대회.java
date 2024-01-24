51c37c9926252e29b8c165b06ace60493b995ae2
이병수 <80665963+gloz0315@users.noreply.github.com>
2024-01-24 09:20:48 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 8.10 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,21 +1,13 @@
 class Solution {
     public String solution(int[] food) {
-        String first = "";
-        String result = "";
-
         StringBuilder sb = new StringBuilder();
 
         for(int i = 1; i < food.length; i++) {
             for(int j = 0; j < food[i]/2; j++) {
-                first += String.valueOf(i);
+                sb.append(i);
             }
         }
-        result = first + "0";
-
-        for(int i = first.length() -1; i >= 0; i--) {
-            result += first.charAt(i);
-        }
-
-        return result;
+        
+        return sb.toString() + "0" + sb.reverse().toString();
     }
 }
