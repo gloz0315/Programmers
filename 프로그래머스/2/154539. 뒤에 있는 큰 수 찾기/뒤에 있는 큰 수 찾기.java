82f15d73b3e28627aa873524d4d65ce6168815c6
이병수 <80665963+gloz0315@users.noreply.github.com>
2024-03-07 09:28:10 +0900

    Update 뒤에 있는 큰 수 찾기.java

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -5,19 +5,24 @@ class Solution {
         int[] answer = new int[numbers.length];
 
         Stack<Integer> stack = new Stack<Integer>();
+        
         for (int i=numbers.length - 1; i>=0; i--) {
+            
             if (stack.size() == 0) {
                 answer[i] = -1;
                 stack.push(numbers[i]);
-            } else {
-                if (stack.peek() <= numbers[i]) {
-                    stack.pop();
-                    i++;
-                } else {
-                    answer[i] = stack.peek();
-                    stack.push(numbers[i]);
-                }
+                continue;
+            } 
+            
+            if(stack.peek() <= numbers[i]) {
+                stack.pop();
+                i++;
+                continue;
             }
+            
+            answer[i] = stack.peek();
+            stack.push(numbers[i]);
+            
         }
 
         return answer;
